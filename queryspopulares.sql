79ad05088284f808dcf3b0fbd74e068957e1d137
Diego Armando Rueda Robles <daruedar@hotmail.com>
2018-03-27 11:46:31 -0400

    Consulta productos por fabricante
    Una consulta de todos los productos    para un fabricante

--- queryspopulares.sql
+++ queryspopulares.sql
@@ -11,3 +11,17 @@ idproducto_fk    es  el nombre    que yo le doy a la  llave foranea
 idproducto   es  el nombre de la columna  a la cual le quiero asignar la  llave foranea
 En REFERENCES productos   es el nombre de la  tabla  a la cual pertenece la llave foranea
 (id)  es el nombre de la columna  en  la  tabla la  cual es   primary ke y  y pasa  a  ser  llave foranea en esta tabla.
+*************************************************************************************************************************
+SELECT productos.nombre, 
+motos.fabricante,
+motos.modelo,
+productos.precio,
+productos.id,
+motos.id,
+repuestosxmoto.idproducto,
+repuestosxmoto.idmoto 
+FROM productos,motos,repuestosxmoto 
+WHERE (motos.fabricante='Yamaha')AND
+(motos.id=repuestosxmoto.idmoto)AND
+(repuestosxmoto.idproducto=productos.id);
+**************************************************************************************************************************
